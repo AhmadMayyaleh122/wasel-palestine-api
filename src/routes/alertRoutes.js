79997af59988138b602d81abf5a9327234a6a6b4
yazee <yazeed.k2016@gmail.com>
2026-04-11 16:42:05 +0200

    ,

--- src/routes/alertRoutes.js
+++ src/routes/alertRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/subscriptions', authMiddleware, alertController.createSubscription);
 router.get('/subscriptions', authMiddleware, alertController.getMySubscriptions);
-
+router.delete('/subscriptions/:id', authMiddleware, alertController.deleteSubscription);
+//hi 
 module.exports = router;
